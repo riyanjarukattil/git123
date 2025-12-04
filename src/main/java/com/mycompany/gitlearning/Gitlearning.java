a0c116537924586a58b41145947f80c4594922e7
JAVA <JAVA@DESKTOP-N5TLAJD>
2025-12-04 10:13:51 +0530

    new file added

---
+++ src/main/java/com/mycompany/gitlearning/Gitlearning.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gitlearning;
+
+/**
+ *
+ * @author JAVA
+ */
+public class Gitlearning {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
